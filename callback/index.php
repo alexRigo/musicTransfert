e378046fbc8918cf6e48a541998ca55b01f28510
Alexis Rigopoulo <alex.rigo@hotmail.fr>
2023-06-19 16:27:41 +0200

    change index callback test

--- callback/index.php
+++ callback/index.php
@@ -1,11 +1,11 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
+/* if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
  
-
 if (isset($_SESSION['source'])) {
     include '../logique/request.php'; 
-} elseif (isset ($_SESSION['destination'])) {
+} elseif (isset($_SESSION['destination'])) {
     include '../logique/transfert.php';
-}
+} */
+include '../logique/transfert.php';
